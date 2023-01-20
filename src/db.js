3f4e0dc75fea89c4636afae3bfa5f6488314d69d
Daniel <daniel_ristoff@hotmail.com>
2023-01-19 21:55:44 -0500

    modify password

--- src/db.js
+++ src/db.js
@@ -1,8 +1,9 @@
 const sequelize = require("sequelize");
-const database = new sequelize("carronamao", "postgres", "root", {
+const database = new sequelize("carronamao", "postgres", "1234", {
   dialect: "postgres",
   host: "localhost",
   port: 5432,
+
 });
 
 //database.sync();
